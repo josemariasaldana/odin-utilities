336223a2e344c79faff87d1e978e92db11f8f152
Fran Gonzalez <francisco.gonzalez.garcia@bisdn.de>
2014-12-10 14:27:31 +0100

    agent.click now includes a scheduler - 2 queues, priority for control over data

--- scripts/start-click.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-cd /root/spring/shared/click
-./tools/click-align/click-align ../scripts/agent.click | ./userlevel/click &> ../scripts/odin.log
-sleep 3
-ifconfig ap up
-#ifconfig mon0 mtu 2200
-
